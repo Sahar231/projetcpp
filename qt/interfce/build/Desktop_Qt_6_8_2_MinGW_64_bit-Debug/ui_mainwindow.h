547873dcc5ea76be3f069171044c4c60faf0e941
RAKIA TRABELSI <rakia.trabelsi@esprit.tn>
2025-02-17 10:49:54 +0000

    Ajout du dossier principal 'src' avec les fichiers

---
+++ qt/interfce/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,297 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDateEdit>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGroupBox *groupBox;
+    QLineEdit *lineEdit;
+    QLabel *label;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QLabel *label_3;
+    QLineEdit *lineEdit_3;
+    QLabel *label_4;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLineEdit *lineEdit_4;
+    QLineEdit *lineEdit_5;
+    QLabel *label_5;
+    QComboBox *comboBox;
+    QLabel *label_8;
+    QPushButton *pushButton_4;
+    QLabel *label_9;
+    QDateEdit *dateEdit;
+    QLabel *label_11;
+    QLineEdit *lineEdit_6;
+    QDateEdit *dateEdit_2;
+    QLabel *label_12;
+    QTableView *tableView;
+    QComboBox *trier;
+    QLineEdit *recherche;
+    QLineEdit *supprimer;
+    QPushButton *pushButton_2;
+    QGroupBox *groupBox_2;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QLabel *label_10;
+    QComboBox *comboBox_3;
+    QPushButton *btnemploye;
+    QPushButton *btnpatient;
+    QPushButton *btnvaccins;
+    QPushButton *btnequipement;
+    QPushButton *btnformation;
+    QPushButton *btnrecherche;
+    QPushButton *logout;
+    QPushButton *logo;
+    QPushButton *rech;
+    QPushButton *pdf;
+    QPushButton *pdf_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1404, 770);
+        MainWindow->setCursor(QCursor(Qt::CursorShape::ArrowCursor));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(180, 30, 221, 621));
+        lineEdit = new QLineEdit(groupBox);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(30, 60, 113, 26));
+        label = new QLabel(groupBox);
+        label->setObjectName("label");
+        label->setGeometry(QRect(10, 30, 121, 20));
+        label_2 = new QLabel(groupBox);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(10, 90, 63, 20));
+        lineEdit_2 = new QLineEdit(groupBox);
+        lineEdit_2->setObjectName("lineEdit_2");
+        lineEdit_2->setGeometry(QRect(30, 110, 113, 26));
+        label_3 = new QLabel(groupBox);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(10, 140, 63, 20));
+        lineEdit_3 = new QLineEdit(groupBox);
+        lineEdit_3->setObjectName("lineEdit_3");
+        lineEdit_3->setGeometry(QRect(30, 170, 113, 26));
+        label_4 = new QLabel(groupBox);
+        label_4->setObjectName("label_4");
+        label_4->setGeometry(QRect(10, 200, 81, 20));
+        label_6 = new QLabel(groupBox);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(10, 260, 81, 20));
+        label_7 = new QLabel(groupBox);
+        label_7->setObjectName("label_7");
+        label_7->setGeometry(QRect(10, 440, 63, 20));
+        lineEdit_4 = new QLineEdit(groupBox);
+        lineEdit_4->setObjectName("lineEdit_4");
+        lineEdit_4->setGeometry(QRect(30, 230, 113, 26));
+        lineEdit_5 = new QLineEdit(groupBox);
+        lineEdit_5->setObjectName("lineEdit_5");
+        lineEdit_5->setGeometry(QRect(30, 290, 113, 26));
+        label_5 = new QLabel(groupBox);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(10, 330, 63, 20));
+        comboBox = new QComboBox(groupBox);
+        comboBox->setObjectName("comboBox");
+        comboBox->setGeometry(QRect(30, 550, 111, 31));
+        label_8 = new QLabel(groupBox);
+        label_8->setObjectName("label_8");
+        label_8->setGeometry(QRect(10, 520, 63, 20));
+        pushButton_4 = new QPushButton(groupBox);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(40, 590, 93, 29));
+        label_9 = new QLabel(groupBox);
+        label_9->setObjectName("label_9");
+        label_9->setGeometry(QRect(0, 530, 101, 20));
+        dateEdit = new QDateEdit(groupBox);
+        dateEdit->setObjectName("dateEdit");
+        dateEdit->setGeometry(QRect(30, 430, 110, 26));
+        label_11 = new QLabel(groupBox);
+        label_11->setObjectName("label_11");
+        label_11->setGeometry(QRect(10, 400, 101, 20));
+        lineEdit_6 = new QLineEdit(groupBox);
+        lineEdit_6->setObjectName("lineEdit_6");
+        lineEdit_6->setGeometry(QRect(30, 360, 113, 26));
+        dateEdit_2 = new QDateEdit(groupBox);
+        dateEdit_2->setObjectName("dateEdit_2");
+        dateEdit_2->setGeometry(QRect(30, 490, 110, 26));
+        label_12 = new QLabel(groupBox);
+        label_12->setObjectName("label_12");
+        label_12->setGeometry(QRect(10, 460, 101, 20));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName("tableView");
+        tableView->setGeometry(QRect(490, 290, 851, 411));
+        trier = new QComboBox(centralwidget);
+        trier->setObjectName("trier");
+        trier->setGeometry(QRect(620, 250, 171, 31));
+        recherche = new QLineEdit(centralwidget);
+        recherche->setObjectName("recherche");
+        recherche->setGeometry(QRect(550, 210, 271, 26));
+        supprimer = new QLineEdit(centralwidget);
+        supprimer->setObjectName("supprimer");
+        supprimer->setGeometry(QRect(810, 250, 113, 26));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(940, 250, 93, 29));
+        groupBox_2 = new QGroupBox(centralwidget);
+        groupBox_2->setObjectName("groupBox_2");
+        groupBox_2->setGeometry(QRect(190, 680, 211, 41));
+        radioButton = new QRadioButton(groupBox_2);
+        radioButton->setObjectName("radioButton");
+        radioButton->setGeometry(QRect(10, 10, 110, 24));
+        radioButton_2 = new QRadioButton(groupBox_2);
+        radioButton_2->setObjectName("radioButton_2");
+        radioButton_2->setGeometry(QRect(100, 10, 81, 24));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName("label_10");
+        label_10->setGeometry(QRect(580, 10, 441, 111));
+        comboBox_3 = new QComboBox(centralwidget);
+        comboBox_3->setObjectName("comboBox_3");
+        comboBox_3->setGeometry(QRect(830, 210, 111, 31));
+        btnemploye = new QPushButton(centralwidget);
+        btnemploye->setObjectName("btnemploye");
+        btnemploye->setGeometry(QRect(0, 100, 161, 81));
+        btnpatient = new QPushButton(centralwidget);
+        btnpatient->setObjectName("btnpatient");
+        btnpatient->setGeometry(QRect(0, 190, 161, 81));
+        btnvaccins = new QPushButton(centralwidget);
+        btnvaccins->setObjectName("btnvaccins");
+        btnvaccins->setGeometry(QRect(0, 280, 161, 81));
+        btnequipement = new QPushButton(centralwidget);
+        btnequipement->setObjectName("btnequipement");
+        btnequipement->setGeometry(QRect(0, 370, 161, 81));
+        btnformation = new QPushButton(centralwidget);
+        btnformation->setObjectName("btnformation");
+        btnformation->setGeometry(QRect(0, 460, 161, 81));
+        btnrecherche = new QPushButton(centralwidget);
+        btnrecherche->setObjectName("btnrecherche");
+        btnrecherche->setGeometry(QRect(0, 550, 161, 81));
+        logout = new QPushButton(centralwidget);
+        logout->setObjectName("logout");
+        logout->setGeometry(QRect(0, 640, 161, 61));
+        logo = new QPushButton(centralwidget);
+        logo->setObjectName("logo");
+        logo->setGeometry(QRect(0, 0, 161, 91));
+        rech = new QPushButton(centralwidget);
+        rech->setObjectName("rech");
+        rech->setGeometry(QRect(950, 210, 91, 31));
+        pdf = new QPushButton(centralwidget);
+        pdf->setObjectName("pdf");
+        pdf->setGeometry(QRect(770, 140, 91, 51));
+        pdf_2 = new QPushButton(centralwidget);
+        pdf_2->setObjectName("pdf_2");
+        pdf_2->setGeometry(QRect(680, 140, 91, 51));
+        MainWindow->setCentralWidget(centralwidget);
+        groupBox->raise();
+        tableView->raise();
+        trier->raise();
+        recherche->raise();
+        supprimer->raise();
+        groupBox_2->raise();
+        label_10->raise();
+        pushButton_2->raise();
+        comboBox_3->raise();
+        btnemploye->raise();
+        btnpatient->raise();
+        btnvaccins->raise();
+        btnequipement->raise();
+        btnformation->raise();
+        btnrecherche->raise();
+        logout->raise();
+        logo->raise();
+        rech->raise();
+        pdf->raise();
+        pdf_2->raise();
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1404, 26));
+        menubar->setTabletTracking(false);
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "Ajouter ", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">Id_vaccin</span></p></body></html>", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">nom_vaccin</span></p></body></html>", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">effet_secondaires</span></p></body></html>", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">composition</span></p><p><br/></p></body></html>", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">date_cr\303\251ation</span></p><p><br/></p></body></html>", nullptr));
+        label_7->setText(QString());
+        label_5->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">quantit\303\251</span></p></body></html>", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">type</span></p></body></html>", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "ajouter", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><br/></p></body></html>", nullptr));
+        label_11->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">date_cr\303\251ation</span></p></body></html>", nullptr));
+        label_12->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">date_peremption</span></p></body></html>", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "supprimer", nullptr));
+        groupBox_2->setTitle(QString());
+        radioButton->setText(QCoreApplication::translate("MainWindow", "Ajouter", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("MainWindow", "Modifier", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:36pt; font-weight:700; color:#ffffff;\">vaccins</span></p></body></html>", nullptr));
+#if QT_CONFIG(whatsthis)
+        btnemploye->setWhatsThis(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">employe</span></p></body></html>", nullptr));
+#endif // QT_CONFIG(whatsthis)
+        btnemploye->setText(QCoreApplication::translate("MainWindow", "employe", nullptr));
+        btnpatient->setText(QCoreApplication::translate("MainWindow", "patient", nullptr));
+        btnvaccins->setText(QCoreApplication::translate("MainWindow", "vaccins", nullptr));
+        btnequipement->setText(QCoreApplication::translate("MainWindow", "equipement", nullptr));
+        btnformation->setText(QCoreApplication::translate("MainWindow", "formation", nullptr));
+        btnrecherche->setText(QCoreApplication::translate("MainWindow", "recherche", nullptr));
+        logout->setText(QString());
+        logo->setText(QString());
+        rech->setText(QString());
+        pdf->setText(QString());
+        pdf_2->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
