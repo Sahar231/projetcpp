0ba6916c76ef9424e87ef8608741eed062988302
sahar riabi <163556094+Sahar231@users.noreply.github.com>
2025-02-18 20:26:41 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     QPalette palette;
-    QPixmap pixmap("C:/Users/hp/Desktop/2A17/qt/interfce/back55.jpg");
+    QPixmap pixmap("c:/Users/Sahar/Bureau/project/interfce/back55.jpg");
     palette.setBrush(QPalette::Window,  pixmap);
     ui->centralwidget->setPalette(palette);
     ui->centralwidget->setAutoFillBackground(true);
@@ -42,39 +42,39 @@ MainWindow::MainWindow(QWidget *parent)
         "}");
 
 
-    ui->logo->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/logo9.png"));
+    ui->logo->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/logo9.png"));
     ui->logo->setIconSize(QSize(150, 150));
     ui->logo->setStyleSheet(commonButtonStyle);
 
 
-    ui->btnemploye->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/employe.png"));
+    ui->btnemploye->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/employe.png"));
     ui->btnemploye->setIconSize(QSize(50, 50));
     ui->btnemploye->setStyleSheet(commonButtonStyle);
-    ui->btnpatient->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/patient.png"));
+    ui->btnpatient->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/patient.png"));
     ui->btnpatient->setIconSize(QSize(50, 50));
     ui->btnpatient->setStyleSheet(commonButtonStyle);
-    ui->btnvaccins->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/vaccins.png"));
+    ui->btnvaccins->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/vaccins.png"));
     ui->btnvaccins->setIconSize(QSize(50, 50));
     ui->btnvaccins->setStyleSheet(commonButtonStyle);
-    ui->btnequipement->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/equipement.png"));
+    ui->btnequipement->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/equipement.png"));
     ui->btnequipement->setIconSize(QSize(50, 50));
     ui->btnequipement->setStyleSheet(commonButtonStyle);
-    ui->btnformation->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/formation.png"));
+    ui->btnformation->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/formation.png"));
     ui->btnformation->setIconSize(QSize(50, 50));
     ui->btnformation->setStyleSheet(commonButtonStyle);
-    ui->btnrecherche->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/recherche.png"));
+    ui->btnrecherche->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/recherche.png"));
     ui->btnrecherche->setIconSize(QSize(50, 50));
-    ui->logout->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/logout1.png"));
+    ui->logout->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/logout1.png"));
     ui->logout->setIconSize(QSize(50, 50));
     ui->btnrecherche->setStyleSheet(commonButtonStyle);
     ui->logout->setStyleSheet(commonButtonStyle);
-    ui->rech->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/reche.png"));
+    ui->rech->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/reche.png"));
     ui->rech->setIconSize(QSize(91, 31));
     ui->rech->setStyleSheet("QPushButton { border: 0px; background: transparent; }");
-    ui->pdf->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/pdf2.png"));
+    ui->pdf->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/pdf2.png"));
     ui->pdf->setIconSize(QSize(91, 51));
     ui->pdf->setStyleSheet("QPushButton { border: 0px; background: transparent; }");
-    ui->pdf_2->setIcon(QIcon("C:/Users/hp/Desktop/2A17/qt/interfce/sta.png"));
+    ui->pdf_2->setIcon(QIcon("c:/Users/Sahar/Bureau/project/interfce/sta.png"));
     ui->pdf_2->setIconSize(QSize(91, 51));
     ui->pdf_2->setStyleSheet("QPushButton { border: 0px; background: transparent; }");
 
@@ -92,7 +92,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->trier->addItem("nom");
     ui->trier->addItem("type");
     ui->trier->addItem("etat");
-    ui->trier->addItem("qnantité");
+    ui->trier->addItem("quantité");
 
 
 
